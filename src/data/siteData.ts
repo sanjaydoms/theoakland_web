d119bef1c317761896d7607c344073b50338a3ae
sanjaydoms <sanjay.garlapenta@domsglobal.co>
2026-08-25 12:59:23 +0530

    Update contact address in siteData.ts

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -9,7 +9,7 @@ export const BRAND = {
 };
 
 export const CONTACT = {
-  address: 'The oakland, Prism mall',
+  address: 'Prism mall, Gachibowli',
   addressLink: 'https://maps.app.goo.gl/nE6qFn7CDSUUPRn47',
   phones: ['+91 9666604356', '+91 9703335683'],
   email: 'customercare@theoakland.in',
